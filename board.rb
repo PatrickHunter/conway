2d4541900852755fe573418a8e38de482e297505
Patrick <pk_hunter@sbcglobal.net>
2013-12-17 16:25:14 -0600

    Working board model, and board test cases.

---
+++ board.rb
@@ -0,0 +1,76 @@
+class Board
+  @grid = Array.new
+  @size = 0
+  
+  def grid
+    return @grid
+  end
+  
+  def grid=(new_grid)
+    @grid = new_grid
+    @size = new_grid.size
+  end
+  
+  def size
+    return @size
+  end
+  
+  def initialize(new_grid)
+    @grid = new_grid
+    @size=@grid.size
+  end
+
+  def step()
+    new_grid = Array.new(size)
+    for i in 0..(size-1)
+      new_grid[i] =Array.new(size)
+    end
+    @grid.each_with_index do |colum, outer_index|
+      colum.each_with_index do |cell, index|        
+        new_grid[outer_index][index] = live?(outer_index,index)
+      end
+    end
+    @grid = new_grid
+  end
+
+  def live?(outer_index, index)
+    live_neighbors = row(outer_index, index)-1
+    if outer_index > 0 then
+      live_neighbors += row(outer_index-1 ,index)
+    end
+    if outer_index +1 <@size then
+      live_neighbors +=row(outer_index+1, index)
+    end
+    
+    if !@grid[outer_index][index] then
+      #live neighbors is off by one if the cell live? is called on is dead
+      if live_neighbors == 2 then
+        return true
+      else
+        return false
+      end
+    end
+    if live_neighbors < 2 or live_neighbors >3 then
+      return false
+    else
+      return true
+    end
+  end
+
+  def row(outer_index, index)
+    count=0
+    if index > 0 && @grid[outer_index][index-1] then
+      count+=1
+    end
+    if @grid [outer_index][index] then
+      count+=1
+    end
+    if index+1 <@size && @grid[outer_index][index+1] then
+      puts @grid[outer_index][index+1]
+      count+=1
+    end
+    return count
+  end
+
+end
+
---
+++ tc_board.rb
@@ -0,0 +1,30 @@
+require_relative 'board'
+require 'test/unit'
+
+class TestBoard < Test::Unit::TestCase
+  
+  def test_setup 
+    sample=[[true,true],[false,false]]
+    assert_equal(sample, Board.new(sample).grid)
+    assert_equal(sample.size, Board.new(sample).size)
+  end
+  
+  "def test_live
+    board = Board.new([[true,true,true],[true,false,true],[true,true,true]])
+    assert(board.live?(0,0))
+    assert(!board.live?(0,1))
+    assert(board.live?(0,2))
+    assert(!board.live?(1,0))
+    assert(!board.live?(1,1))
+    assert(!board.live?(1,2))
+    assert(board.live?(2,0))
+    assert(!board.live?(2,1))
+    assert(board.live?(2,2))
+end"
+  
+  def test_step
+    board = Board.new([[true,false,true],[false,true,true],[true,true,false]])
+    board.step
+    assert_equal([[false,false,true],[false,false,true],[true,true,true]],board.grid)
+  end
+end
